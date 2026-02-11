9a11c4e1a8c26f867b1024e46d9055459053a638
Thecoughingknight <jabrokecool@gmail.com>
2026-02-11 13:49:19 -0600

    Update main.js

--- GameFiles/js/main.js
+++ GameFiles/js/main.js
@@ -1,1 +1,19 @@
+const App = {
+    nodes: [],
 
+    init() {
+        console.log("App Initialized");
+    },
+
+    addNode(id, x = 100, y = 100) {
+        const newNode = new Node(id, x, y);
+        this.nodes.push(newNode);
+    },
+
+    clear() {
+        this.nodes.forEach(n => n.element.remove());
+        this.nodes = [];
+    }
+};
+
+window.onload = () => App.init();
