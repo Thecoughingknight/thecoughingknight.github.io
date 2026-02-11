c219ef4b83489be691b6e1f3c9f4f308bc9ea852
Thecoughingknight <jabrokecool@gmail.com>
2026-02-11 13:49:04 -0600

    Update storage.js

--- GameFiles/js/storage.js
+++ GameFiles/js/storage.js
@@ -1,1 +1,15 @@
+const StorageManager = {
+    saveCanvas() {
+        const data = App.nodes.map(n => ({ id: n.id, x: n.x, y: n.y }));
+        localStorage.setItem('logic_sim_data', JSON.stringify(data));
+        alert("Saved!");
+    },
 
+    loadCanvas() {
+        const data = JSON.parse(localStorage.getItem('logic_sim_data'));
+        if (data) {
+            App.clear();
+            data.forEach(d => App.addNode(d.id, d.x, d.y));
+        }
+    }
+};
